e0268cddf63d6d95bedffa907d6326e74bb77646
Diego Oliveira <diego.brivia@tecnobank.com.br>
2025-09-26 07:58:19 -0300

    Atualização e melhorias no gerenciamento de produtos
    Foram realizadas diversas alterações no projeto, incluindo:
    - Adição de novos serviços e métodos no `ProductsController.cs` para suporte a CRUD de produtos, categorias e imagens.
    - Criação de novas views (`Create`, `Edit`, `Details`, `Delete`) para manipulação de produtos.
    - Atualizações no layout do site (`_Layout.cshtml`) e no estilo (`site.css`) para melhorar a aparência e responsividade.
    - Inclusão de imagens padrão e suporte a exibição de imagens no catálogo.
    - Ajustes em arquivos de validação e remoção de referências obsoletas.
    - Atualização de arquivos de licença e dependências para refletir mudanças recentes.

--- CleanCode.WebUI/Controllers/ProductsController.cs
+++ CleanCode.WebUI/Controllers/ProductsController.cs
@@ -1,14 +1,20 @@
-﻿using CleanCode.Application.Interfaces;
+﻿using CleanCode.Application.Dtos;
+using CleanCode.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanCode.WebUI.Controllers;
 
 public class ProductsController(
     ILogger<ProductsController> logger,
-    IProductService productService) : Controller
+    IProductService productService,
+    ICategoryService categoryService,
+    IWebHostEnvironment environment) : Controller
 {
     private readonly ILogger<ProductsController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     private readonly IProductService _productService = productService;
+    private readonly ICategoryService _categoryService = categoryService;
+    private readonly IWebHostEnvironment _environment = environment;
 
     [HttpGet]
     public async Task<IActionResult> Index()
@@ -17,5 +23,105 @@ public class ProductsController(
 
         var entity = await _productService.GetAllAsync();
         return View(entity);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.CategoryId = new SelectList(await _categoryService.GetAllAsync(), "Id", "Name");
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductDto entity)
+    {
+        if (ModelState.IsValid)
+        {
+            CancellationToken cancellationToken = CancellationToken.None;
+            await _productService.AddAsync(entity, cancellationToken);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(entity);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return BadRequest();
+
+        var entity = await _productService.GetByIdAsync(id.Value);
+
+        ViewBag.CategoryId = new SelectList(await _categoryService.GetAllAsync(), "Id", "Name", entity!.CategoryId);
+
+        if (entity == null)
+            return NotFound();
+
+        return View(entity);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ProductDto entity)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                CancellationToken cancellationToken = CancellationToken.None;
+                await _productService.UpdateAsync(entity, cancellationToken);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(entity);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+            return BadRequest();
+
+        var entity = await _productService.GetByIdAsync(id.Value);
+
+        if (entity == null)
+            return NotFound();
+
+        var wwwroot = _environment.WebRootPath;
+        var image = Path.Combine(wwwroot, $"images\\{entity.Image}");
+        var exists = System.IO.File.Exists(image);
+        ViewBag.ImageExist = exists;
+
+        return View(entity);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return BadRequest();
+
+        var entity = await _productService.GetByIdAsync(id.Value);
+
+        if (entity == null)
+            return NotFound();
+
+        return View(entity);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        CancellationToken cancellationToken = CancellationToken.None;
+        await _productService.DeleteAsync(id, cancellationToken);
+
+        return RedirectToAction(nameof(Index));
     }
 }
